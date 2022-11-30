f6940f439a1bd6e37ca106ae44443b016e628241
Kobe <kobecuppens@hotmail.com>
2022-11-30 11:41:26 +0100

    fix: epub to pdf

--- apps/convert/services/FileConverter.py
+++ apps/convert/services/FileConverter.py
@@ -40,31 +40,8 @@ class FileConverter:
         open(file_name, "wb").write(r.content)
         # Get conversion path
         path = get_conversion_path(conversion)
-        # Convert from epub to html
-        output_file = convert_from_epub_to_html(file_name, path)
-        # Convert from html to pdf
-        output_file = convert_from_html_to_pdf(output_file)
-        return output_file
-
-
-def convert_from_epub_to_html(input, output):
-    import pypandoc
-
-    pypandoc.convert_file(
-        input,
-        format="epub",
-        to="html5",
-        extra_args=["--read=epub", f"--extract-media={output}", "--wrap=none"],
-        encoding="utf-8",
-        outputfile=output + "/" + input.replace(".epub", ".html"),
-        filters=None,
-        verify_format=True,
-    )
-    return output + "/" + input.replace(".epub", ".html")
-
-
-def convert_from_html_to_pdf(input):
-    from weasyprint import HTML
-
-    HTML(input).write_pdf(input.replace(".html", ".pdf"))
-    return input.replace(".html", ".pdf")
+        # Convert
+        cmd = ["ebook-convert", file_name, path]
+        p = subprocess.Popen(cmd)
+        p.communicate()
+        return f"{path}{file_name.replace('.docx', '.pdf')}"
