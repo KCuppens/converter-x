4c5ef89ca45e7b9e5be2057b0fb4814ad12a924b
Kobe <kobecuppens@hotmail.com>
2022-11-30 12:29:24 +0100

    fix: testcase

--- apps/convert/tests/services/test_file_converter.py
+++ apps/convert/tests/services/test_file_converter.py
@@ -57,3 +57,16 @@ class FileConverterTestCase(CustomGraphQLTestCase):
             self.assertTrue(path)
             os.remove(path)
             self.assertFalse(os.path.exists(path))
+
+    def test_convert_from_gif_to_mp4(self):
+        self.initial_file.file = "test_files/test_gif.gif"
+        self.initial_file.save(update_fields=["file"])
+        if sys.platform == "win32":
+            assert True
+        else:
+            path = FileConverter().convert_from_epub_to_pdf(self.conversion)
+            self.assertTrue(path.split(".")[-1] == "gif")
+            self.assertTrue(os.path.exists(path))
+            self.assertTrue(path)
+            os.remove(path)
+            self.assertFalse(os.path.exists(path))
