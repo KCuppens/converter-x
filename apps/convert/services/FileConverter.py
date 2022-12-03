8df874c67f625e2dbfc387520187c2cf0bf4951c
Kobe <kobecuppens@hotmail.com>
2022-12-03 06:26:12 +0100

    imp: extra functioning

--- apps/convert/services/FileConverter.py
+++ apps/convert/services/FileConverter.py
@@ -1,20 +1,13 @@
 import subprocess
 
-import requests
 from pdf2docx import parse
 
-from apps.convert.utils import get_conversion_path
-from apps.initial_files.utils import get_unique_file_name
+from apps.convert.utils import get_initial_file_for_conversion
 
 
 class FileConverter:
     def convert_from_doc_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        # Download file and save
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
         # Convert
         cmd = ["soffice", "--convert-to", "pdf", "--outdir", path, file_name]
         p = subprocess.Popen(cmd)
@@ -22,12 +15,7 @@ class FileConverter:
         return f"{path}{file_name.replace('.doc', '.pdf')}"
 
     def convert_from_docx_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
         # Convert
         cmd = ["soffice", "--convert-to", "pdf", "--outdir", path, file_name]
         p = subprocess.Popen(cmd)
@@ -35,83 +23,59 @@ class FileConverter:
         return f"{path}{file_name.replace('.docx', '.pdf')}"
 
     def convert_from_epub_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.epub', '.pdf')}"
         # Convert
-        cmd = ["ebook-convert", file_name, f"{path}{file_name.replace('.epub', '.pdf')}"]
+        cmd = ["ebook-convert", file_name, output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.epub', '.pdf')}"
+        return output
 
     def convert_from_gif_to_mp4(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.gif', '.mp4')}"
         # Convert
         from moviepy.editor import VideoFileClip
 
         video_clip = VideoFileClip(file_name)
-        video_clip.write_videofile(f"{path}{file_name.replace('.gif', '.mp4')}")
-        return f"{path}{file_name.replace('.gif', '.mp4')}"
+        video_clip.write_videofile(output)
+        return output
 
     def convert_from_m4a_to_mp3(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.m4a', '.mp3')}"
         # Convert
         from pydub import AudioSegment
 
         sound = AudioSegment.from_file(file_name)
-        sound.export(f"{path}{file_name.replace('.m4a', '.mp3')}")
-        return f"{path}{file_name.replace('.m4a', '.mp3')}"
+        sound.export(output)
+        return output
 
     def convert_from_jpg_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.jpg', '.pdf')}"
         # Convert
         from PIL import Image
 
         image = Image.open(file_name)
         image_convert = image.convert("RGB")
-        image_convert.save(f"{path}{file_name.replace('.jpg', '.pdf')}")
-        return f"{path}{file_name.replace('.jpg', '.pdf')}"
+        image_convert.save(output)
+        return output
 
     def convert_from_jpg_to_png(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.jpg', '.png')}"
         # Convert
         from PIL import Image
 
         image = Image.open(file_name)
         image = image.convert("RGB")
-        image.save(f"{path}{file_name.replace('.jpg', '.png')}")
-        return f"{path}{file_name.replace('.jpg', '.png')}"
+        image.save(output)
+        return output
 
     def convert_from_heic_to_jpg(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.heic', '.jpg')}"
         # Convert
         import pyheif
         from PIL import Image
@@ -125,111 +89,69 @@ class FileConverter:
             heif_file.mode,
             heif_file.stride,
         )
-        data.save(f"{path}{file_name.replace('.heic', '.jpg')}")
-        return f"{path}{file_name.replace('.heic', '.jpg')}"
+        data.save(output)
+        return output
 
     def convert_from_mkv_to_mp4(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mkv', '.mp4')}"
         # Convert
-        cmd = [
-            "ffmpeg",
-            "-i",
-            file_name,
-            "-codec",
-            "copy",
-            f"{path}{file_name.replace('.mkv', '.mp4')}",
-        ]
+        cmd = ["ffmpeg", "-i", file_name, "-codec", "copy", output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.mkv', '.mp4')}"
+        return output
 
     def convert_from_mov_to_mp4(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mov', '.mp4')}"
         # Convert
-        cmd = ["ffmpeg", "-i", file_name, f"{path}{file_name.replace('.mov', '.mp4')}"]
+        cmd = ["ffmpeg", "-i", file_name, output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.mov', '.mp4')}"
+        return output
 
     def convert_from_mp3_to_m4a(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp3', '.m4a')}"
         # Convert
         from pydub import AudioSegment
 
         sound = AudioSegment.from_file(file_name)
-        sound.export(f"{path}{file_name.replace('.mp3', '.m4a')}")
-        return f"{path}{file_name.replace('.mp3', '.m4a')}"
+        sound.export(output)
+        return output
 
     def convert_from_mp3_to_mp4(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp3', '.mp4')}"
         # Convert
-        cmd = [
-            "ffmpeg",
-            "-i",
-            file_name,
-            "-acodec",
-            "copy",
-            f"{path}{file_name.replace('.mp3', '.mp4')}",
-        ]
+        cmd = ["ffmpeg", "-i", file_name, "-acodec", "copy", output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.mp3', '.mp4')}"
+        return output
 
     def convert_from_mp3_to_wav(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp3', '.wav')}"
         # Convert
         from pydub import AudioSegment
 
         sound = AudioSegment.from_mp3(file_name)
-        sound.export(f"{path}{file_name.replace('.mp3', '.wav')}")
-        return f"{path}{file_name.replace('.mp3', '.wav')}"
+        sound.export(output)
+        return output
 
     def convert_from_mp4_to_gif(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp4', '.gif')}"
         # Convert
         from moviepy.editor import VideoFileClip
 
         video_clip = VideoFileClip(file_name)
-        video_clip.write_gif(f"{path}{file_name.replace('.mp4', '.gif')}")
-        return f"{path}{file_name.replace('.mp4', '.gif')}"
+        video_clip.write_gif(output)
+        return output
 
     def convert_from_mp4_to_mkv(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp4', '.mkv')}"
         # Convert
         cmd = [
             "ffmpeg",
@@ -237,19 +159,15 @@ class FileConverter:
             file_name,
             "-codec",
             "copy",
-            f"{path}{file_name.replace('.mp4', '.mkv')}",
+            output,
         ]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.mp4', '.mkv')}"
+        return output
 
     def convert_from_mp4_to_mov(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.mp4', '.mov')}"
         # Convert
         cmd = [
             "ffmpeg",
@@ -259,56 +177,39 @@ class FileConverter:
             "copy",
             "-vcodec",
             "copy",
-            f"{path}{file_name.replace('.mp4', '.mov')}",
+            output,
         ]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.mp4', '.mov')}"
+        return output
 
     def convert_from_pdf_to_epub(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.pdf', '.epub')}"
         # Convert
-        cmd = ["ebook-convert", file_name, f"{path}{file_name.replace('.pdf', '.epub')}"]
+        cmd = ["ebook-convert", file_name, output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.pdf', '.epub')}"
+        return output
 
     def convert_from_pdf_to_doc(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.pdf', '.doc')}"
         # Convert
         from pdf2docx import parse
 
-        parse(file_name, f"{path}{file_name.replace('.pdf', '.doc')}")
-        return f"{path}{file_name.replace('.pdf', '.doc')}"
+        parse(file_name, output)
+        return output
 
     def convert_from_pdf_to_docx(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.pdf', '.docx')}"
         # Convert
-        parse(file_name, f"{path}{file_name.replace('.pdf', '.docx')}")
-        return f"{path}{file_name.replace('.pdf', '.docx')}"
+        parse(file_name, output)
+        return output
 
     def convert_from_pdf_to_jpg(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
         # Convert
         from pdf2image import convert_from_path
 
@@ -329,12 +230,7 @@ class FileConverter:
         return f"{path}images.zip"
 
     def convert_from_pdf_to_png(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
         # Convert
         import fitz
 
@@ -357,54 +253,37 @@ class FileConverter:
         return f"{path}images.zip"
 
     def convert_from_pdf_to_pptx(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.pdf', '.pptx')}"
         # Convert
-        cmd = ["pdf2pptx", file_name, "-o", f"{path}{file_name.replace('.pdf', '.pptx')}"]
+        cmd = ["pdf2pptx", file_name, "-o", output]
         p = subprocess.Popen(cmd)
         p.communicate()
-        return f"{path}{file_name.replace('.pdf', '.pptx')}"
+        return output
 
     def convert_from_png_to_jpg(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.png', '.jpg')}"
         # Convert
         from PIL import Image
 
         image = Image.open(file_name)
-        image.save(f"{path}{file_name.replace('.png', '.jpg')}")
-        return f"{path}{file_name.replace('.png', '.jpg')}"
+        image.save(output)
+        return output
 
     def convert_from_png_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.png', '.pdf')}"
         # Convert
         from PIL import Image
 
         image = Image.open(file_name)
         image = image.convert("RGB")
-        image.save(f"{path}{file_name.replace('.png', '.pdf')}")
-        return f"{path}{file_name.replace('.png', '.pdf')}"
+        image.save(output)
+        return output
 
     def convert_from_pptx_to_pdf(self, conversion):
-        initial_file = conversion.initial_file
-        r = requests.get(initial_file.file.url)
-        file_name = get_unique_file_name(initial_file.file)
-        open(file_name, "wb").write(r.content)
-        # Get conversion path
-        path = get_conversion_path(conversion)
+        file_name, path = get_initial_file_for_conversion(conversion)
         # Convert
 
         cmd = ["soffice", "--convert-to", "pdf", "--outdir", path, file_name]
@@ -425,3 +304,13 @@ class FileConverter:
         sound = AudioSegment.from_mp3(file_name)
         sound.export(f"{path}{file_name.replace('.wav', '.mp3')}")
         return f"{path}{file_name.replace('.wav', '.mp3')}"
+
+    def convert_from_wav_to_mp3(self, conversion):
+        file_name, path = get_initial_file_for_conversion(conversion)
+        output = f"{path}{file_name.replace('.wav', '.mp3')}"
+        # Convert
+        from pydub import AudioSegment
+
+        sound = AudioSegment.from_mp3(file_name)
+        sound.export(output)
+        return output
